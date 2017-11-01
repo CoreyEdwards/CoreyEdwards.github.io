39ba2827b6457f2619620e0332c3d6a24eec5853
CoreyEdwards <31259317+CoreyEdwards@users.noreply.github.com>
2017-10-31 20:29:32 -0500

    fixed the pop ups

--- homepage/javascript.js
+++ homepage/javascript.js
@@ -11,27 +11,29 @@ document.write(
 	
 //validation of no whitespaces
 function validate()
-{
+	{
 
-	var firstname= document.getElementById("firstname").value;
-    var lastname = document.getElementById("lastname").value;
-    var email=     document.getElementById("email").value;
-    var message=   document.getElementById("message").value;
+		var firstname= document.getElementById("firstname").value;
+		var lastname = document.getElementById("lastname").value;
+		var email=     document.getElementById("email").value;
+		var message=   document.getElementById("message").value;
 	
-	if (firstname == "" || !isNaN(firstname)) {
-        alert ("You have left \"First Name\" empty. Please enter your First name,or NA to remain anonymous.");
-        return false;
-    }
-    if (lastname == "" || !isNaN(lastname)) {
-        alert ("You have left \"Last Name\" empty. Please enter your Last name,or NA to remain anonymous.");
-        return false;
-    }
-    if (email == "" || !isNaN(email)) {
-        alert ("You have left \"E-mail Address\" empty. Please enter your E-mail address,or put NA if you do not have one. ");
-        return false;
-    }
-    if (message == "") {
-        alert ("Whoops! \n You forgot to leave a message! \nOur Customer service depends on your input! ");
-        return false;
+		if (firstname == "") {
+			alert ("You have left \"First Name\" empty. Please enter your First name,or NA to remain anonymous.");
+			return false;
+		}
+		if (lastname == "") {
+			alert ("You have left \"Last Name\" empty. Please enter your Last name,or NA to remain anonymous.");
+			return false;
+		}
+		if (email == "") {
+			alert ("You have left \"E-mail Address\" empty. Please enter your E-mail address,or put NA if you do not have one. ");
+			return false;
+		}
+		if (message == "") {
+			alert ("Whoops! \n You forgot to leave a message! \nOur Customer service depends on your input! ");
+			return false;
     }
 }
+//selecting the form to validate
+	document.getElementById("contactusform").onsubmit = validate;
