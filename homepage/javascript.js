333e87bb7894cdd9425d6f618944fd80adfaf287
CoreyEdwards <31259317+CoreyEdwards@users.noreply.github.com>
2017-11-14 20:10:20 -0600

    added slideshow to homepage

--- homepage/javascript.js
+++ homepage/javascript.js
@@ -40,6 +40,7 @@ function validate()
 	document.getElementById("contactusform").onsubmit = validate;
 	
 //***********************************************************************************************************
+//drop menu stuff
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
@@ -60,4 +61,34 @@ window.onclick = function(event) {
       }
     }
   }
+}
+
+//*****************************************************************************************
+// slideshow stuff
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
 }
